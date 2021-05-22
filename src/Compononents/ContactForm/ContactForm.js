7ca3031953da0ea2055d42f560b7fcbc1c366144
Emmanuel_Sewah <sewah2012@gmail.com>
2021-05-22 04:11:01 +0000

    add client section and contact section on homepage

---
+++ src/Compononents/ContactForm/ContactForm.js
@@ -0,0 +1,124 @@
+import './ContactForm.css'
+import React, { useState } from 'react'
+import { Button, Grid, Snackbar, TextField } from '@material-ui/core'
+
+const ContactForm = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+	const [open, setOpen] = useState(false);
+
+	const handleClose = (e, reason) => {
+		if (reason === 'clickaway') {
+			return (
+				console.log("working")
+			)
+		}
+		setOpen(false)
+	}
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault()
+		// db.collection('contact').add({
+		// 	name: name,
+		// 	email: email,
+		// 	message: message,
+		// })
+		// 	.then(() => {
+		// 		setOpen(true);
+		// 	})
+		// 	.catch((error) => {
+		// 		alert(error.message)
+		// 	})
+
+		setName("")
+		setEmail("")
+		setMessage("")
+
+	}
+	// function Alert(props) {
+	// 	return <MuiAlert elevation={6} variant='filled' {...props} />;
+	// }
+
+	return (
+		<div>
+			<br /><br />
+
+			<form noValidate=''>
+				<Grid
+					container
+					direction='column'
+					justify='center'
+					alignItems='center'
+				>
+					<Grid container item>
+						<TextField
+							require={true}
+							id='name'
+							label='Name'
+							variant='outlined'
+							autoComplete='new-name'
+							fullWidth
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</Grid>
+					<br />
+					<br />
+					<Grid container item>
+						<TextField
+							required
+							id='email'
+							label='Email'
+							variant='outlined'
+							autoComplete='new-email'
+							fullWidth
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</Grid>
+					<br />
+					<br />
+					<Grid container item>
+						<TextField
+							required
+							id='message'
+							label='Message'
+							fullWidth
+							multiline
+							rows={4}
+							value={message}
+							variant='outlined'
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+					</Grid>
+					<br />
+					<br />
+					<br />
+					<br />
+					<Grid container item>
+						<Button
+							variant='contained'
+							fullWidth
+							color='primary'
+							onClick={onSubmitHandler}
+						>
+							Submit
+              </Button>
+					</Grid>
+				</Grid>
+			</form>
+			<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+				{/* <Alert onClose={handleClose} severity='success'>
+					message submitted successfully!
+          		</Alert> */}
+
+				  message submitted successfully!
+			</Snackbar>
+		</div>
+
+	)
+}
+
+
+export default ContactForm
--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -3,14 +3,19 @@ import React from 'react'
 import NavBar from '../../Compononents/NavBar/NavBar'
 import { Button } from '@material-ui/core'
 import Service from '../../Compononents/services/Service'
+import ContactForm from '../../Compononents/ContactForm/ContactForm'
 
+// icons
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
 const Home = () => {
 	const styles = {
 		backgroundImage: 'url("assets/images/banner1.jpg")',
 		backgroundRepeat: 'no-repeat',
 		backgroundSize: 'cover',
 		bacgroundColor: 'blue',
-		width: '100%',
+		// width: '100v',
 		height: '600px',
 
 
@@ -22,13 +27,13 @@ const Home = () => {
 			<section style={styles} className='home__topBanner' >
 				<div className='home__topBanner-text'>
 					<h1>Welcome to M and H Global Services </h1>
-					<Button 
-						variant='contained' 
-						color='secondary' 
-						// style={{width: '10rem', height: '5rem'}}
+					<Button
+						variant='contained'
+						color='secondary'
+					// style={{width: '10rem', height: '5rem'}}
 					>
 						Discover-us
-					
+
 					</Button>
 				</div>
 			</section>
@@ -37,15 +42,15 @@ const Home = () => {
 				<img src='assets/images/businessAnalysis.svg' alt='abt_home' />
 				<div className='home__about-content'>
 					<h2>About - us </h2>
-					<p>Lorem ipsum dolor sit amet, 
-						consectetur adipiscing elit. 
-						Nulla congue nunc ut nulla placerat 
-						sollicitudin. Proin feugiat fermentum 
-						ipsum, eget congue lacus viverra ut. Donec venenatis mauris in ultricies rutrum. 
-						Nullam blandit ut magna sit amet ornare. 
-						Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus 
-						et est convallis malesuada ut vel magna.
+					<p>Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.
+					Nulla congue nunc ut nulla placerat
+					sollicitudin. Proin feugiat fermentum
+					ipsum, eget congue lacus viverra ut. Donec venenatis mauris in ultricies rutrum.
+					Nullam blandit ut magna sit amet ornare.
+					Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus
+					et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus
+					et est convallis malesuada ut vel magna.
 					</p>
 					<Button variant='outlined' color='primary'> see more ...</Button>
 				</div>
@@ -53,47 +58,107 @@ const Home = () => {
 
 			<section className='home__services'>
 				<div className='home__services-content'>
-				<h2>A votre service</h2>
-				<p>
-			
-						Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus 
+					<h2>A votre service</h2>
+					<p>
+
+						Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus
+						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus
 				</p>
 				</div>
 				<div className='home__services-items'>
 					<Service
 						sDescription='Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus ' 
+						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus '
 						sTitle='Creation de Site Web'
 						sEmblem='assets/images/services.svg'
-						
-						
 					/>
 					<Service
 						sDescription='Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus ' 
+						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus '
 						sTitle='Creation de Site Web'
 						sEmblem='assets/images/services.svg'
-						
-						
 					/>
 					<Service
 						sDescription='Integer ullamcorper euismod lacinia. Curabitur id tempus elit. Orci varius natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus ' 
+						et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum et tellus '
 						sTitle='Creation de Site Web'
 						sEmblem='assets/images/services.svg'
-						
-						
 					/>
 				</div>
-				
 
 			</section>
-			<h1>nos références clients </h1>
-			<h1>contact us section ...  </h1>
+
+			<section className='home__clients'>
+				<h2>nos références clients</h2>
+				<div className='home__clients-logos'>
+					<ul className='clients'>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+						<li className='client'>
+							<a href='#' target='_bank'>
+								<img src='assets/images/client.jpg' alt='client' />
+							</a>
+						</li>
+					</ul>
+				</div>
+
+			</section>
+
+
+			<section id='homeContact'>
+
+				<div className='home__contact-info'>
+					<h2> Contact Information </h2>
+					<p>Rendez vous chez nous: </p>
+					<LocationOnIcon /><p>Rabat, Medina - Morocco </p> <PhoneIcon /> <a href='tel:+212632952071'>+212 632 952 071</a> <EmailIcon /> <a href='' >sewah2012@gmail.com</a>
+				</div>
+				<div className='home__contact'>
+					<div className='home__contact-form'>
+						<ContactForm />
+					</div>
+
+					<div className='home__contact-map'>
+						MAP
+					</div>
+				</div>
+			</section>
+
 			<h1>footer </h1>
-			
-		</div>
+
+		</div >
 	)
 }
 
